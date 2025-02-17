6f624905250d35b7cbd8e791a6a186548616c67f
Vasilis Tsavalias <v.tsavalias@gmail.com>
2025-02-17 22:24:59 +0200

    fixed the powershell commands and generalized them to work in every machine + improved folder structure

--- front_end/src/components/App.jsx
+++ front_end/src/components/App.jsx
@@ -1,51 +1,54 @@
-import { useState } from 'react'; // Only import useState
-import './App.css';
-import background from '../assets/forrest.jpg';
-import logo from '../assets/FD-removebg.png';
-import Uploader from './Uploader';
-import Results from './Results';
-import axios from 'axios';
+import { useState } from "react"; // Only import useState
+import "./App.css";
+import background from "../assets/forrest.jpg";
+import logo from "../assets/FD-removebg.png";
+import Uploader from "./Uploader";
+import Results from "./Results";
+import axios from "axios";
 
 function App() {
   const [cnnResults, setCnnResults] = useState(null);
   const [yoloResults, setYoloResults] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [showResults, setShowResults] = useState(false); // Control when to show results
+  const [showResults, setShowResults] = useState(false);
 
   const handleImageUpload = async (imageFile, modelType) => {
     setLoading(true);
     setError(null);
-    // Clear previous results of the *opposite* type:
-    if (modelType === 'cnn') {
-        setYoloResults(null);
+
+    if (modelType === "cnn") {
+      setYoloResults(null);
     } else {
-        setCnnResults(null);
+      setCnnResults(null);
     }
 
     const formData = new FormData();
-    formData.append('image', imageFile);
-    formData.append('model_type', modelType);
+    formData.append("image", imageFile);
+    formData.append("model_type", modelType);
 
     try {
-      const response = await axios.post('http://127.0.0.1:8080/predict', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
+      const response = await axios.post(
+        "http://127.0.0.1:8080/predict",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
 
-      if (modelType === 'cnn') {
+      if (modelType === "cnn") {
         setCnnResults(response.data);
       } else {
         setYoloResults(response.data);
       }
-      setShowResults(true); // Show results after successful upload
-
+      setShowResults(true);
     } catch (error) {
       if (error.response) {
-        setError(error.response.data.error || 'An error occurred.');
+        setError(error.response.data.error || "An error occurred.");
       } else {
-        setError('An error occurred.');
+        setError("An error occurred.");
       }
     } finally {
       setLoading(false);
@@ -54,42 +57,61 @@ function App() {
 
   const handleGoBack = () => {
     setShowResults(false);
-    setCnnResults(null);   // Clear results
+    setCnnResults(null);
     setYoloResults(null);
   };
 
   return (
     <>
       <div className="flex justify-center items-center h-screen">
-        <img src={background} alt="background" className="w-screen h-screen object-cover" />
+        <img
+          src={background}
+          alt="background"
+          className="w-screen h-screen object-cover"
+        />
         <div className="absolute w-full h-full flex flex-col justify-center items-center">
-          <img className="absolute md:left-5 top-15 md:top-5 z-1 aspect-1 w-28" src={logo} alt="logo" />
           <div className="absolute h-full w-full bg-white opacity-20 p-8 rounded-lg" />
 
           {!showResults ? (
-            // Show uploaders when showResults is false
-            <div className="flex flex-row space-x-4 w-full justify-center">
-              <div className="flex flex-col items-center">
-                <Uploader onImageUpload={(file) => handleImageUpload(file, 'cnn')} modelType="cnn" />
-                {/* {cnnLoading && <p>Processing CNN Image...</p>}  Unified loading message */}
+            <div className="flex flex-col space-x-4 py-4 justify-center bg-white w-full md:w-[70%] h-full md:h-[65%] container drop-shadow-xl">
+              <div className="flex justify-center items-center space-x-2">
+                <img className="w-20" src={logo} alt="logo" />
+                <h1 className="text-center text-xl font-bold">
+                  Fire Detection
+                </h1>
+              </div>
+
+              <h3 className="text-center text-lg mx-auto w-[60%] py-2">
+                If you suspect there might be a fire nearby, please let us
+                determine it for you and upload a photo that clearly shows the
+                site!
+              </h3>
+              <div className="flex flex-col justify-center items-center">
+                <Uploader
+                  onImageUpload={(file) => handleImageUpload(file, "cnn")}
+                  modelType="cnn"
+                />
               </div>
               <div className="flex flex-col items-center">
-                <Uploader onImageUpload={(file) => handleImageUpload(file, 'yolo')} modelType="yolo" />
-                {/* {yoloLoading && <p>Processing YOLO Image...</p>}  Unified loading message */}
+                <Uploader
+                  onImageUpload={(file) => handleImageUpload(file, "yolo")}
+                  modelType="yolo"
+                />
               </div>
             </div>
           ) : (
-            // Show results and back button when showResults is true
             <>
-              <button onClick={handleGoBack} className="absolute top-4 left-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              <button
+                onClick={handleGoBack}
+                className="absolute top-4 left-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              >
                 Back
               </button>
-              {/* Display Results based on which model was used */}
               {cnnResults && <Results results={cnnResults} />}
               {yoloResults && <Results results={yoloResults} />}
             </>
           )}
-            {loading && <p>Processing...</p>} {/* Unified loading message */}
+          {loading && <p>Processing...</p>}
           {error && <p className="error">{error}</p>}
         </div>
       </div>
--- front_end/src/components/Uploader.jsx
+++ front_end/src/components/Uploader.jsx
@@ -1,6 +1,6 @@
-import  { useRef, useState } from "react";
+import { useRef, useState } from "react";
 import uploadIcon from "../assets/upload.svg";
-import PropTypes from 'prop-types'; // Import PropTypes
+import PropTypes from "prop-types"; // Import PropTypes
 
 function Uploader({ onImageUpload, modelType }) {
   const fileInputRef = useRef(null);
@@ -20,55 +20,48 @@ function Uploader({ onImageUpload, modelType }) {
     onImageUpload(selectedFile, modelType); // Pass the file AND modelType
   };
 
-    const getLabel = () => {
-        return modelType === "cnn" ? "Fire classification" : "smoke detection";
-    }
+  const getLabel = () => {
+    return modelType === "cnn" ? "Fire classification" : "Smoke detection";
+  };
 
   return (
-    <form className="w-[85%] md:w-[50%] h-[60%]" onSubmit={handleSubmit}>
-    <div className="flex flex-col justify-center items-center  w-full container  py-10 px-[10%] space-y-8">
-      <h1 className="z-10 font-bold text-xl md:text-3xl text-black text-center">
-        {getLabel()}
-      </h1>
-      <h3 className="text-center">
-        If you suspect there might be a fire nearby, please let us determine
-        it for you and upload a photo that clearly shows the site!
-      </h3>
-        <>
-          <input
-            type="file"
-            ref={fileInputRef}
-            className="hidden"
-            id={`fileUpload-${modelType}`} // Unique ID
-            onChange={handleFileChange}
-            accept="image/*"
-          />
-          <label
-            htmlFor={`fileUpload-${modelType}`} // Use unique ID
-            className="z-10 flex flex-col items-center justify-center w-full h-32 border-2 border-dashed border-gray-800 rounded-lg cursor-pointer hover:bg-gray-100 transition duration-200 ease-in-out"
-          >
-            <img className="w-10 h-10" src={uploadIcon} alt="Upload Icon" />
-            <span className="mt-2 text-sm">
-              {selectedFile ? `Selected: ${selectedFile.name}` : "Click to Upload"}
-            </span>
-          </label>
-        </>
+    <form className="w-[80%] h-full" onSubmit={handleSubmit}>
+      <div className="flex flex-col justify-center items-center w-full py-5 px-[5%] space-y-6">
+        <input
+          type="file"
+          ref={fileInputRef}
+          className="hidden"
+          id={`fileUpload-${modelType}`} // Unique ID
+          onChange={handleFileChange}
+          accept="image/*"
+        />
+        <label
+          htmlFor={`fileUpload-${modelType}`} // Use unique ID
+          className="z-10 flex flex-col items-center justify-center w-full h-24 border-2 border-dashed border-gray-800 rounded-lg cursor-pointer hover:bg-gray-100 transition duration-200 ease-in-out"
+        >
+          <img className="w-10 h-10" src={uploadIcon} alt="Upload Icon" />
+          <span className="mt-2 text-sm">
+            {selectedFile
+              ? `Selected: ${selectedFile.name}`
+              : "Click to Upload"}
+          </span>
+        </label>
 
-      <button
-        className="z-10 w-full cursor-pointer bg-black p-3 text-white hover:bg-white hover:text-black rounded-lg transition duration-200 ease-in-out"
-        type="submit"
-      >
-      Examine
-      </button>
-    </div>
-  </form>
+        <button
+          className="z-10 w-full cursor-pointer bg-black p-3 text-white hover:bg-white hover:text-black rounded-lg transition duration-200 ease-in-out"
+          type="submit"
+        >
+          {getLabel()}
+        </button>
+      </div>
+    </form>
   );
 }
 
 // Add PropTypes for validation
 Uploader.propTypes = {
   onImageUpload: PropTypes.func.isRequired,
-  modelType: PropTypes.oneOf(['cnn', 'yolo']).isRequired,
+  modelType: PropTypes.oneOf(["cnn", "yolo"]).isRequired,
 };
 
 export default Uploader;
